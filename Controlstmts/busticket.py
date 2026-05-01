9178281b5e40e27c71fb5bce5d36decfb2df0e89
Annamalai1995 <s.annamalai1995@gmail.com>
2026-05-01 10:52:48 +0530

    controls

---
+++ Controlstmts/busticket.py
@@ -0,0 +1,51 @@
+#IF ..IF..ELSE ,ELIF,NESTED IF
+# age =18
+# weight=50
+# if age >= 18:
+#     print("VAlid age ")
+# else:
+#     print("not valid ")    
+
+#College Data
+# person=input("Tell us who are you?")
+# if person=="student" or person =="staff" or person=="Hod":
+#     print("We let you inside the campus")
+# else:
+#     print("You are not inside the campus")    
+
+#Bus ticket
+
+# Age =int(input("Enter the Age"))
+# if Age <=10:
+#     print("HAlf ticket")
+# elif Age >=18:
+#     print("Full Ticket")
+# elif Age >=60:
+#     print("Senior Citizen you have go to free Ticket")   
+# else:
+#     print("Thanks for coming")    
+
+#Nested IF
+#syntax:
+# if(condition):
+#     if(condition):
+#         stmts
+#      else:
+#         stmts
+# else:
+#     stmts;
+
+
+#Theme Park
+age=int(input("Enter The Age"))
+if age>=18:
+    print("welcome to Black Thunder")
+    print("where you can have thriling rides")
+    gender=input("Enter The Gender")
+    if gender =="male" or gender=="Female":
+        print("water games")
+    else:
+        print("Dry Games")    
+else:
+    print("Miss iT again")
+
