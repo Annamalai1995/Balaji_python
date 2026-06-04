1a9752bae1f1563a289a974f77f75658fa3cea01
Annamalai1995 <s.annamalai1995@gmail.com>
2026-06-04 09:44:29 +0530

    Function

---
+++ Function/demo_fun1.py
@@ -0,0 +1,11 @@
+def College():
+    COllege_name=input("ENter The College NAme")
+    if COllege_name =="PSG":
+        print("Best Placemnet in Tamilnadu")
+    elif COllege_name =="Kumaraguru":
+        print    ("High Pakage salary ")
+    elif COllege_name =="Karpagam":
+        print("Better college")
+    else:
+        print("Not eligible")
+College()
