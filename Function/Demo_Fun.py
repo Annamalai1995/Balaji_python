d37d567cfb1cac385698855512449c3ef1e993a7
Annamalai1995 <s.annamalai1995@gmail.com>
2026-06-04 09:21:26 +0530

    function

---
+++ Function/Demo_Fun.py
@@ -0,0 +1,24 @@
+def Balaji():    #function creation
+    print("Haii Balaji")
+Balaji()    #Function Calling
+print("hai ")
+
+#with return 
+def Add():
+    cosmo=1005
+    beta=458
+    sum=cosmo+beta
+    return sum
+print("The ADDING VALUES",Add())
+
+#without Return type
+def sum():
+    gamma=145
+    alpha=789
+    add=gamma*alpha
+    print("Multiply Values",add)
+sum()    
+#with parameter
+def name(place):
+    print("cooling places",place)
+name("OOTY")    
